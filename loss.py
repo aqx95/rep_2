348252457af916b1374df4533169b06cc1ddaf6d
aqx95 <angqx95@gmail.com>
2021-04-15 14:59:45 +0800

    update focal loss

--- loss.py
+++ loss.py
@@ -61,10 +61,12 @@ class IoULoss(nn.Module):
 
 
 class FocalLoss(nn.Module):
-    def __init__(self, weight=None, size_average=True):
+    def __init__(self, alpha=0.8, gamma=2.0, weight=None, size_average=True):
         super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
 
-    def forward(self, inputs, targets, alpha=0.8, gamma=2.0, smooth=1):
+    def forward(self, inputs, targets, smooth=1):
         #flatten label and prediction tensors
         inputs = inputs.view(-1)
         targets = targets.view(-1)
@@ -72,7 +74,7 @@ class FocalLoss(nn.Module):
         #first compute binary cross-entropy
         BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
         BCE_EXP = torch.exp(-BCE)
-        focal_loss = alpha * (1-BCE_EXP)**gamma * BCE
+        focal_loss = self.alpha * (1-BCE_EXP)**self.gamma * BCE
 
         return focal_loss
 
